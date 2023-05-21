04791f8bb1558ef230b29122f688b7df837e44e5
Stefan Velev <sdvelev@gmail.com>
2023-05-21 14:02:42 +0300

    Added main structure of project

---
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/EventPlanningAndCollaborationToolApplication.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.web.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanningAndCollaborationToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPlanningAndCollaborationToolApplication.class, args);
+    }
+
+}
