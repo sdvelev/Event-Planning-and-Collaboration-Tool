6a0345815a085bfb3d8cacf255f72046bda8f62b
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-05-21 21:05:19 +0300

    Add Task model structure

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/model/Task.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/model/Task.java
@@ -1,13 +1,40 @@
 package bg.sofia.uni.fmi.web.project.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @Column(length = 255, nullable = false)
+    private String name;
+
+    @Column(length = 255, nullable = false)
+    private String description;
+
+    @Column
+    private LocalDateTime dueDate;
+
+    @Column
+    private TaskProgress taskProgress;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "collaborator_id", nullable = false)
+    private Collaborator collaborator;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
 }
