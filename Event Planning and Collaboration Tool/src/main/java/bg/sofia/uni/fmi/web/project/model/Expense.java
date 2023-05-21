7cabc594e957cb00b815d76d3cb3b700b409be1c
Stefan Velev <sdvelev@gmail.com>
2023-05-21 16:23:10 +0300

    feat: add expense model as entity without relationships

---
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/model/Expense.java
@@ -0,0 +1,35 @@
+package bg.sofia.uni.fmi.web.project.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import jdk.jfr.Enabled;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    @NotNull
+    private BigDecimal price;
+}
