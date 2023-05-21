b555d8bdcf8414dfb1dc5bf31daecc7800756eba
Stefan Velev <sdvelev@gmail.com>
2023-05-21 15:31:02 +0300

    feat: add user model as entity without relationships

---
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/model/User.java
@@ -0,0 +1,40 @@
+package bg.sofia.uni.fmi.web.project.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    @NotNull
+    private String username;
+
+    @Column(name = "password")
+    @NotNull
+    private String password;
+
+    @Column(name = "email", unique = true)
+    @NotNull
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "age")
+    private Integer age;
+}
