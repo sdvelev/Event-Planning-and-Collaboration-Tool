9a195c49872f8f6d3183a554e974059f064525f1
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-05-21 20:51:28 +0300

    Create Contract model

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/model/Vendor.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/model/Vendor.java
@@ -1,4 +1,15 @@
 package bg.sofia.uni.fmi.web.project.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Vendor {
+
 }
