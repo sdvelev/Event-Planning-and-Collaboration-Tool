5885a853405c5bd7502071743c663d10715b3ada
Stefan Velev <sdvelev@gmail.com>
2023-05-27 16:28:02 +0300

    feat: add repository for participant

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/repository/ParticipantRepository.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/repository/ParticipantRepository.java
@@ -1,10 +1,10 @@
 package bg.sofia.uni.fmi.web.project.repository;
 
-import bg.sofia.uni.fmi.web.project.model.Collaborator;
+import bg.sofia.uni.fmi.web.project.model.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CollaboratorRepository extends JpaRepository<Collaborator, Long> {
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 
 }
