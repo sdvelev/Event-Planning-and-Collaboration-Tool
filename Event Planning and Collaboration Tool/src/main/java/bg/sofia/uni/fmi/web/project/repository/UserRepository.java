7da74a9ceface6a09c96af7acf4d9b41083e18a5
Stefan Velev <sdvelev@gmail.com>
2023-05-29 10:08:40 +0300

    fix: change return type

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/repository/UserRepository.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/repository/UserRepository.java
@@ -4,13 +4,14 @@ import bg.sofia.uni.fmi.web.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
-    Optional<User> findByUsernameAndPassword(String username, String password);
+    List<User> findByEmail(String email);
+    List<User> findByUsername(String username);
+    List<User> findByUsernameAndPassword(String username, String password);
 
 }
