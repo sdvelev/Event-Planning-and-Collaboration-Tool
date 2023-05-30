7d4585a8dcb1cea10205e4a24deec39aaf8dedc3
Stefan Velev <sdvelev@gmail.com>
2023-05-30 07:19:33 +0300

    refactor: delete expense repository and invitation repository

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/repository/InvitationRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.web.project.repository;
-
-import bg.sofia.uni.fmi.web.project.model.Invitation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvitationRepository extends JpaRepository<Invitation, Long> {
-}
