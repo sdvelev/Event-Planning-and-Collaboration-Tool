4db7a90be4f71222123c4183a6793c51dd9ba8e0
Stefan Velev <sdvelev@gmail.com>
2023-06-11 23:35:56 +0300

    feat: add dto for event

---
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/dto/ExpenseDto.java
@@ -0,0 +1,39 @@
+package bg.sofia.uni.fmi.web.project.dto;
+
+import bg.sofia.uni.fmi.web.project.enums.ExpenditureCategory;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExpenseDto {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("expenditure_category")
+    private ExpenditureCategory expenditureCategory;
+
+    @JsonProperty("amount")
+    private BigDecimal amount;
+
+    @JsonProperty("approved")
+    private boolean approved;
+
+    @JsonProperty("associated_event")
+    private EventDto associatedEventDto;
+}
