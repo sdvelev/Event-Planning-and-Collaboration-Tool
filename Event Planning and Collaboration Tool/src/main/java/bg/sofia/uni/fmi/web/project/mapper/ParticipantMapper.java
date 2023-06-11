4b5d55c6fb158cbf077cca3dec5d16ad2481d042
Stefan Velev <sdvelev@gmail.com>
2023-06-11 21:11:53 +0300

    fix: add annotation details for using mapper in another mapper

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/mapper/ParticipantMapper.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/mapper/ParticipantMapper.java
@@ -5,11 +5,13 @@ import bg.sofia.uni.fmi.web.project.dto.ParticipantDto;
 import bg.sofia.uni.fmi.web.project.model.Event;
 import bg.sofia.uni.fmi.web.project.model.Participant;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {UserMapper.class, EventMapper.class})
 public interface ParticipantMapper {
 
     ParticipantMapper INSTANCE = Mappers.getMapper(ParticipantMapper.class);
@@ -17,49 +19,4 @@ public interface ParticipantMapper {
     ParticipantDto toDto (Participant model);
 
     Participant toEntity(ParticipantDto dto);
-
-//    final private UserMapper userMapper;
-//    final private EventMapper eventMapper;
-//
-//    @Autowired
-//    public ParticipantMapper(UserMapper userMapper, EventMapper eventMapper) {
-//        this.userMapper = userMapper;
-//        this.eventMapper = eventMapper;
-//    }
-//
-//    public ParticipantDto toDto(Participant entity) {
-//
-//        ParticipantDto toReturnDto = new ParticipantDto();
-//
-//        if (entity.getAssociatedUser() != null) {
-//            toReturnDto.setAssociatedUserDto(new UserMapper().toDto(entity.getAssociatedUser()));
-//        }
-//
-//        if (entity.getAssociatedEvent() != null) {
-//            toReturnDto.setAssociatedEventDto(eventMapper.toDto(entity.getAssociatedEvent()));
-//        }
-//
-//        toReturnDto.setId(entity.getId());
-//        toReturnDto.setUserRole(entity.getUserRole());
-//
-//        return toReturnDto;
-//    }
-//
-//    public Participant toEntity(ParticipantDto participantDto) {
-//
-//        Participant toReturnEntity = new Participant();
-//
-//        toReturnEntity.setId(participantDto.getId());
-//        toReturnEntity.setUserRole(participantDto.getUserRole());
-//
-//        if (participantDto.getAssociatedUserDto() != null) {
-//            toReturnEntity.setAssociatedUser(userMapper.toEntity(participantDto.getAssociatedUserDto()));
-//        }
-//
-//        if (participantDto.getAssociatedEventDto() != null) {
-//            toReturnEntity.setAssociatedEvent(eventMapper.toEntity(participantDto.getAssociatedEventDto()));
-//        }
-//
-//        return toReturnEntity;
-//    }
 }
