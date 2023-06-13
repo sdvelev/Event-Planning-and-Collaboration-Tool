4d5458b01f4f2587f2340f9b544e79228fb5a792
Stefan Velev <sdvelev@gmail.com>
2023-06-13 13:10:56 +0300

    refactor: remove unused import statements in mappers

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/mapper/BudgetMapper.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/mapper/BudgetMapper.java
@@ -1,9 +1,7 @@
 package bg.sofia.uni.fmi.web.project.mapper;
 
 import bg.sofia.uni.fmi.web.project.dto.BudgetDto;
-import bg.sofia.uni.fmi.web.project.dto.ExpenseDto;
 import bg.sofia.uni.fmi.web.project.model.Budget;
-import bg.sofia.uni.fmi.web.project.model.Expense;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/mapper/ExpenseMapper.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/mapper/ExpenseMapper.java
@@ -1,9 +1,7 @@
 package bg.sofia.uni.fmi.web.project.mapper;
 
 import bg.sofia.uni.fmi.web.project.dto.ExpenseDto;
-import bg.sofia.uni.fmi.web.project.dto.ParticipantDto;
 import bg.sofia.uni.fmi.web.project.model.Expense;
-import bg.sofia.uni.fmi.web.project.model.Participant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
