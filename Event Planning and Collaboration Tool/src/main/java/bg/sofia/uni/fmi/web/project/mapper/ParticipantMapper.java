2896873f6c5286eb66378550bf174fd461118c09
Stefan Velev <sdvelev@gmail.com>
2023-06-13 13:00:08 +0300

    refactor: remove unused import statements

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/mapper/ParticipantMapper.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/mapper/ParticipantMapper.java
@@ -1,15 +1,10 @@
 package bg.sofia.uni.fmi.web.project.mapper;
 
-import bg.sofia.uni.fmi.web.project.dto.EventDto;
 import bg.sofia.uni.fmi.web.project.dto.ParticipantDto;
-import bg.sofia.uni.fmi.web.project.model.Event;
 import bg.sofia.uni.fmi.web.project.model.Participant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class, EventMapper.class})
 public interface ParticipantMapper {
