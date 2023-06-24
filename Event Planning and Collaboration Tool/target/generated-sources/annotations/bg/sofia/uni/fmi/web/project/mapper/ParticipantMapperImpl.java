3442b835d335f606ea67bd5a60c196a93c8a35af
Stefan Velev <sdvelev@gmail.com>
2023-06-24 11:49:14 +0300

    refactor: updated implementation

--- Event Planning and Collaboration Tool/target/generated-sources/annotations/bg/sofia/uni/fmi/web/project/mapper/ParticipantMapperImpl.java
+++ Event Planning and Collaboration Tool/target/generated-sources/annotations/bg/sofia/uni/fmi/web/project/mapper/ParticipantMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-21T08:54:37+0300",
+    date = "2023-06-24T10:52:39+0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19 (Oracle Corporation)"
 )
 @Component
