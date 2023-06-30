85ebfcb5ce8a1213c91431a221db1f996946fb84
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-06-30 12:24:55 +0300

    After merge commit

--- Event Planning and Collaboration Tool/target/generated-sources/annotations/bg/sofia/uni/fmi/web/project/mapper/GuestMapperImpl.java
+++ Event Planning and Collaboration Tool/target/generated-sources/annotations/bg/sofia/uni/fmi/web/project/mapper/GuestMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-30T12:09:38+0300",
+    date = "2023-06-30T12:22:32+0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.1 (Oracle Corporation)"
 )
 @Component
