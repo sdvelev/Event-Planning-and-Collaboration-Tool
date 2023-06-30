af3a6240237ad7ebe875e04913380785332a9d3b
Stefan Velev <sdvelev@gmail.com>
2023-06-30 10:11:04 +0300

    feat: added new query method for retrieving users by associated user

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/repository/ParticipantRepository.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/repository/ParticipantRepository.java
@@ -1,6 +1,7 @@
 package bg.sofia.uni.fmi.web.project.repository;
 
 import bg.sofia.uni.fmi.web.project.model.Participant;
+import bg.sofia.uni.fmi.web.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,5 @@ public interface ParticipantRepository extends JpaRepository<Participant, Long> 
 
     Optional<Participant> findByIdAndDeletedFalse(Long id);
     List<Participant> findAllByDeletedFalse();
+    List<Participant> findAllByAssociatedUserAndDeletedFalse(User user);
 }
