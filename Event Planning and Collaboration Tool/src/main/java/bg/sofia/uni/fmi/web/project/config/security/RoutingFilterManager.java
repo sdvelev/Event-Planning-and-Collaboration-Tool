f8c4abf87d234e86dfd00b7980bc10806cf81cf9
Stefan Velev <sdvelev@gmail.com>
2023-06-30 15:25:18 +0300

    refactor: updated classes

--- Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/config/security/RoutingFilterManager.java
+++ Event Planning and Collaboration Tool/src/main/java/bg/sofia/uni/fmi/web/project/config/security/RoutingFilterManager.java
@@ -1,12 +1,12 @@
 package bg.sofia.uni.fmi.web.project.config.security;
 
-import bg.sofia.uni.fmi.web.project.model.User;
-import io.swagger.models.HttpMethod;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
